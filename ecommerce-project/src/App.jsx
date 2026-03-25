426c29c5b91eae4f538a66c3316ca934bc781d30
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2026-03-25 16:47:06 +0700

    add order page and split header to component

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import { Homepage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Homepage />} /> {/* index = path="/" */}
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
