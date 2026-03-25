0397e09d0c40b8603fb756323bdd012dc8c46f1a
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2026-03-25 22:33:14 +0700

    add server proxy config

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -10,12 +10,12 @@ export function Homepage() {
   useEffect(() => {
     // Nếu không dùng useEffect, thì khi component được render, nó sẽ thực hiện lệnh axios.get() mỗi lần render, dẫn đến việc gửi nhiều yêu cầu đến server và gây ra hiệu suất kém.
     // Sử dụng useEffect với một mảng rỗng [] làm dependency sẽ đảm bảo rằng lệnh axios.get() chỉ được thực hiện một lần khi component được mount, tránh việc gửi nhiều yêu cầu không cần thiết đến server.
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       console.log(response.data);
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+    axios.get("/api/cart-items").then((response) => {
       console.log(response.data);
       setCartItems(response.data);
     });
